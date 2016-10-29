06eae265716018811f24baf50e68e6414cc455f0
Chuey <jabarri1@gmail.com>
2016-10-29 12:17:23 -0700

    sss

--- lib/ultibot/commands/sim.rb
+++ lib/ultibot/commands/sim.rb
@@ -15,8 +15,16 @@ module ArmoryBot
           "On a scale of 1-10 it's probably an 8.56345",
           "You're simming less than the tank, probably forgot to flask",
           "Sims say we should put warlock gateway over there instead",
-          "We should try Time Warping during the transitioning so Luci & Wicks can run faster"]
-  event.respond "Let me sim that for you...hmm..."
+          "We should try Time Warping during the transitioning so Luci & Wicks can run faster",
+          presim = ["https://cdn.discordapp.com/attachments/100313873264963584/239567548671066112/WIN_20161022_21_46_14_Pro.jpg",
+                    "Let me sim that for you...hmm...",
+                    "I can calculate that for you...",
+                    "If I run this through my custom script, and import it into simcraft...",
+                    "Using my custom APL, I can gather tons of information for you...",
+                    "I know it's not a 100% accurate but...",
+                    "I'm going to test this against the top 100 in the world...",
+                    "Compared to other people with your class and spec..."]
+  event.respond "#{presim.sample}"
   event.respond "#{sim.sample}"
 
       end
