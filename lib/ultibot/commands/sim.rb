8aa56e32ac0f63bddd6814eeae252472724c783b
Chuey <jabarri1@gmail.com>
2016-10-29 12:40:32 -0700

    s

--- lib/ultibot/commands/sim.rb
+++ lib/ultibot/commands/sim.rb
@@ -2,7 +2,7 @@ module ArmoryBot
   module Commands
     module Sim
       extend Discordrb::Commands::CommandContainer
-      command(:sim, bucket: :sim) do |event|
+      command(:sim, bucket: :sim, rate_limit_message: "You've simmed too much, wait %time%") do |event|
         sim = ["Your current item is a bit better...disenchant maybe?", 
           "I think I could use that, actually. Give me!", 
           "That's roughly a 2 percent increase.", 
@@ -16,13 +16,13 @@ module ArmoryBot
           "You're simming less than the tank, probably forgot to flask",
           "Sims say we should put warlock gateway over there instead",
           "We should try Time Warping during the transitioning so Luci & Wicks can run faster",
-          "That's an 32.3333%/ damage increase, repeating of course."]
+          "That's an 32.3333% damage increase, repeating of course."]
           presim = ["https://cdn.discordapp.com/attachments/100313873264963584/239567548671066112/WIN_20161022_21_46_14_Pro.jpg",
                     "Let me sim that for you...hmm...",
                     "I can calculate that for you...",
                     "If I run this through my custom script, and import it into simcraft...",
                     "Using my custom APL, I can gather tons of information for you...",
-                    "I know it's not a 100%/ accurate but...",
+                    "I know it's not a 100% accurate but...",
                     "I'm going to test this against the top 100 in the world...",
                     "Compared to other people with your class and spec..."]
   event.respond "#{presim.sample}"
