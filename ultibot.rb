452a5d5e358ec96fc63417e66e6ecdcc2fe4f4b7
Jesus Barrientos <jabarri1@gmail.com>
2020-11-11 17:10:52 -0800

    removed important info

--- ultibot.rb
+++ ultibot.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new(token: 'MjQxNzUzODY5NTY1MDM0NDk3.CvWeXQ.xMQm6iy1rksL3jT1z4hVdMBQBV0', application_id: '241753869565034497', prefix: '!')
+bot = Discordrb::Commands::CommandBot.new(token: '', application_id: '', prefix: '!')
 
 bot.command([:sim, :Sim, :ulti]) do |event|
 	sim = ["Your current item is a bit better...disenchant maybe?", "I think I could use that, actually. Give me!", "That's roughly a 2 percent increase.", "Those are your best stats so probably good for you."]
